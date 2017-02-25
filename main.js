88f5a9e2870660c4d63f28fe16f66407a98e2040
Shanmugam MPl <git@shan.mp>
2017-02-25 06:32:45 -0600

    Integrate hardtap event from myo to play chords

--- main.js
+++ main.js
@@ -5,9 +5,19 @@ console.log("banououououou");
 
 console.log(Myo);
 
+Myo.connect();
+
 Myo.on('connected', function(){
   console.log('connected!', this.id)
 });
+
+
+
+
+
+console.log("boop")
+
+
 var chords = {
   I:   [48, 52, 55, 60, 64, 67, 72],
   ii:  [50, 53, 57, 62, 65, 69, 74],
@@ -17,8 +27,20 @@ var chords = {
   vi:  [45, 48, 52, 57, 60, 64, 69],
   vii: [47, 50, 53, 59, 62, 65, 71]
 };
-
 var chordMap = ['I', 'ii', 'iii', 'IV', 'vi', 'vii'];
+
+var chord = getChord();
+
+function getChord() {
+    var l = chordMap.length;
+    return chords[chordMap[(5 ^ l) % (l - 1)]];
+  }
+
+  function getNote() {
+    var note = chord[5];
+    return (5 % 14 == 0) && (m % 3 == 0) ? note + 1 : note;
+  }
+
 
 window.onload = function () {
 	MIDI.loadPlugin({
@@ -33,8 +55,19 @@ window.onload = function () {
 			var velocity = 127; // how hard the note hits
 			// play the note
 			MIDI.setVolume(0, 127);
-			MIDI.noteOn(0, note, velocity, delay);
-			MIDI.noteOff(0, note, delay + 0.75);
+			//MIDI.noteOn(0, note, velocity, delay);
+			//MIDI.noteOff(0, note, delay + 0.75);
+			Myo.on('tap', function(){
+				MIDI.noteOn(0, getNote(5), 127, 0.01);
+				console.log("tap!!!")
+			});
+
+			Myo.on('hard_tap', function(){
+				MIDI.noteOn(0, 71, 127, 0.01);
+				console.log("hardtap!!!")
+			});
+
+
 		}
 	});
 };
