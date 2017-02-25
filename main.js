ae305c3bfe2a39b57e27575475ec21008060253b
Shanmugam MPl <git@shan.mp>
2017-02-25 16:52:27 -0600

    Add :Musical Keyboard: scales at the start and keytonotelookuptable

--- main.js
+++ main.js
@@ -17,13 +17,18 @@ Myo.on('connected', function(){
   
 
 	});
-
+//A0:21,A1:33,A2:45,A3:57,A4:69,A5:81,A6:93,A7:105,Ab1:32,Ab2:44,Ab3:56,Ab4:68,Ab5:80,Ab6:92,Ab7:104,
+//B0:23,B1:35,B2:47,B3:59,B4:71,B5:83,B6:95,B7:107,Bb0:22,Bb1:34,Bb2:46,Bb3:58,Bb4:70,Bb5:82,Bb6:94,Bb7:106,
+//C1:24,C2:36,C3:48,C4:60,C5:72,C6:84,C7:96,C8:108,
+//D1:26,D2:38,D3:50,D4:62,D5:74,D6:86,D7:98,Db1:25,Db2:37,Db3:49,Db4:61,Db5:73,Db6:85,Db7:97,
+//E1:28,E2:40,E3:52,E4:64,E5:76,E6:88,E7:100,Eb1:27,Eb2:39,Eb3:51,Eb4:63,Eb5:75,Eb6:87,Eb7:99,
+//F1:29,F2:41,F3:53,F4:65,F5:77,F6:89,F7:101,
+//G1:31,G2:43,G3:55,G4:67,G5:79,G6:91,G7:103,Gb1:30,Gb2:42,Gb3:54,Gb4:66,Gb5:78,Gb6:90,Gb7:102,
 	function song(){
 		// console.log(Math.ceil(avg))
 		console.log(velocity)
 		
 		MIDI.noteOn(0, Math.ceil(avg), velocity, 0.001);
-
 	}
 
 	Myo.on('emg', function(data){
@@ -114,6 +119,7 @@ window.onload = function () {
 	MIDI.loadPlugin({
 		soundfontUrl: "./soundfont/",
 		instrument: "synth_drum",
+		instrument: "acoustic_grand_piano",
 		//instrument: "acoustic_guitar",
 		onprogress: function(state, progress) {
 			console.log(state, progress);
@@ -123,10 +129,26 @@ window.onload = function () {
 			var note = 50; // the MIDI note
 			var velocity = 10; // how hard the note hits
 			// play the note
-			MIDI.programChange(0,118);
 			MIDI.setVolume(0, 127);
-			MIDI.noteOn(0, 50, 127, 0.1);
-			MIDI.noteOn(0, avg, velocity, delay);
+
+			MIDI.noteOn(0, 57 , 127, 0.1);
+			MIDI.noteOff(0, 57, 0.7);
+			MIDI.noteOn(0, 59 , 127, 0.2);
+			MIDI.noteOff(0, 59, 0.7);
+			MIDI.noteOn(0, 60 , 127, 0.3);
+			MIDI.noteOff(0, 60, 0.7);
+			MIDI.noteOn(0, 62 , 127, 0.4);
+			MIDI.noteOff(0, 62, 0.7);
+			MIDI.noteOn(0, 64 , 127, 0.5);
+			MIDI.noteOff(0, 64, 0.7);
+			MIDI.noteOn(0, 65 , 127, 0.6);
+			MIDI.noteOff(0, 65, 0.7);
+			MIDI.noteOn(0, 67 , 127, 0.7);
+			MIDI.noteOff(0, 67, 0.7);
+			
+
+
+			// MIDI.noteOn(0, avg, velocity, delay);
 			//MIDI.noteOn(0, note, velocity, delay);
 			//MIDI.noteOff(0, note, delay + 0.75);
 			Myo.on('fist', function(){
