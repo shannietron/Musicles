6ee4cc235115ee612ed396d6599d53005f194648
Shanmugam MPl <git@shan.mp>
2017-02-25 05:58:48 -0600

    Gave up on using require MIDI, doesn't work for some reason I don't understand :(

--- bundle.js
+++ bundle.js
@@ -1,5 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Myo = require('Myo');
+
+
 console.log("banououououou");
 
 console.log(Myo);
@@ -7,6 +9,36 @@ console.log(Myo);
 Myo.on('connected', function(){
   console.log('connected!', this.id)
 });
+var chords = {
+  I:   [48, 52, 55, 60, 64, 67, 72],
+  ii:  [50, 53, 57, 62, 65, 69, 74],
+  iii: [52, 55, 59, 64, 67, 71, 76],
+  IV:  [41, 45, 48, 53, 57, 60, 65],
+  V:   [43, 47, 50, 55, 59, 62, 67],
+  vi:  [45, 48, 52, 57, 60, 64, 69],
+  vii: [47, 50, 53, 59, 62, 65, 71]
+};
+
+var chordMap = ['I', 'ii', 'iii', 'IV', 'vi', 'vii'];
+
+window.onload = function () {
+	MIDI.loadPlugin({
+		soundfontUrl: "./soundfont/",
+		instrument: "acoustic_grand_piano",
+		onprogress: function(state, progress) {
+			console.log(state, progress);
+		},
+		onsuccess: function() {
+			var delay = 0; // play one note every quarter second
+			var note = 50; // the MIDI note
+			var velocity = 127; // how hard the note hits
+			// play the note
+			MIDI.setVolume(0, 127);
+			MIDI.noteOn(0, note, velocity, delay);
+			MIDI.noteOff(0, note, delay + 0.75);
+		}
+	});
+};
 },{"Myo":2}],2:[function(require,module,exports){
 (function(){
 	var Socket, myoList = {};
